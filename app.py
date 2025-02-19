c7739897347f85a7918678395d5a58de1aa55965
souji123133 <gsouji37@gmail.com>
2025-02-19 12:03:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-import boto4
-import dev1
-def lambda_handler(event, context):
-  client = boto3.client('ec2')
-  response = client.run_instances(
-    ImageId='ami-0614680123427b75e',
-    InstanceType='t2.micro',
-    KeyName='mykeymumbai',
-    MaxCount=1,
-    MinCount=2
-)
+print("Hello, world!")
+print("Welcome to Python programming.")
+
